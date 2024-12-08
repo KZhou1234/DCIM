66b3315efffadbd4c48f4416cfc7b6a1c5d580d2
Ubuntu <ubuntu@ip-172-31-33-230.ec2.internal>
2024-12-08 03:49:05 +0000

    userdata for endpoint variables

--- terraform/EC2/main.tf
+++ terraform/EC2/main.tf
@@ -19,7 +19,16 @@ resource "aws_instance" "app_server" {
   key_name = "shafee-jenkins-keypair"
   subnet_id = var.private_subnet[count.index % length(var.private_subnet)]
   
-  user_data = base64encode(file("${path.module}/deploy.sh"))
+  user_data = base64encode(file("${path.module}/deploy.sh", {
+    rds_endpoint = var.rds_endpoint,
+    redis_endpoint = var.redis_endpoint,
+    docker_compose = templatefile("${path.root}/../docker/docker-compose.yml", {
+      rds_endpoint = var.rds_endpoint,
+      redis_endpoint = var.redis_endpoint
+
+      #run_migrations = count.index == 0 ? "true" : "false"
+    }),
+  }))
 
   tags = {
     Name = "ralph_app_az${count.index + 1}"
